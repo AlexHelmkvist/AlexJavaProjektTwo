e86abd9e78d85147e0d672105172d096fa8fdf2f
Alex <alex33376@hotmail.com>
2023-10-31 11:54:56 +0300

    Tenta

---
+++ TextReaderTest.java
@@ -0,0 +1,22 @@
+package TentaProgramering;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextReaderTest {
+
+    @org.junit.jupiter.api.Test
+    void textReaderLine() {
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLineCount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCharCount() {
+    }
+}
